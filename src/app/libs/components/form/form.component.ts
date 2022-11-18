101dcfa1f864c6353d336a33a1090f85e691da8e
nataliabalakhashvili <40176916+nataliabalakhashvili@users.noreply.github.com>
2022-11-18 14:57:45 +0400

    feat - form controls, form group and validation required

--- src/app/libs/components/form/form.component.ts
+++ src/app/libs/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormTitles } from "./entity/form.interface";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -8,6 +9,14 @@ import { FormTitles } from "./entity/form.interface";
 })
 export class FormComponent implements OnInit {
   @Input() formTitles: FormTitles;
+
+  form: FormGroup = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      mail: new FormControl(null, [Validators.required]),
+      subject: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required)
+    }
+  )
 
   constructor() { }
 
