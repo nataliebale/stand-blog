e199756569f70da853f4aadb9be26c6e54e8a080
nataliabalakhashvili <40176916+nataliabalakhashvili@users.noreply.github.com>
2022-11-18 13:33:12 +0400

    feat - forms module

---
+++ src/app/libs/components/form/form.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormComponent } from './form.component';
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
+
+@NgModule({
+  declarations: [
+    FormComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class FormModule { }
