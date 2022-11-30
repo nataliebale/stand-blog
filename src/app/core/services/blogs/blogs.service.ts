0d1a08dafcadcdced9727c7a024502f300f5e8d7
nataliabalakhashvili <40176916+nataliabalakhashvili@users.noreply.github.com>
2022-11-30 23:58:13 +0400

    feat - some changes

--- src/app/core/services/blogs/blogs.service.ts
+++ src/app/core/services/blogs/blogs.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogsService {
+  private _jsonURL = 'src/api/data.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+    });
+  }
+  public getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
 }
