b70ff7d4fd466cec9601998e4ab34a3c1e2c0638
nataliabalakhashvili <40176916+nataliabalakhashvili@users.noreply.github.com>
2022-12-02 00:03:52 +0400

    feat - error handling

--- src/app/core/services/blogs/blogs.service.ts
+++ src/app/core/services/blogs/blogs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, Observable, tap } from "rxjs";
 import { ICard } from "../../../libs/components/card/entity/card.interface";
 
 @Injectable({
@@ -14,7 +14,19 @@ export class BlogsService {
       console.log(data);
     });
   }
-  public getJSON(): Observable<any> {
-    return this.http.get<ICard[]>(this.blogsUrl);
+  public getJSON(): Observable<ICard[]> {
+    return this.http.get<ICard[]>(this.blogsUrl).pipe(
+      tap(data => console.log(11111, data)),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse){
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occured: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
   }
 }
