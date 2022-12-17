cfceb232874ada3723cff030973c733fb6b35b1b
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2022-12-17 19:55:09 +0400

    feat - change JSON data

--- src/app/core/services/tags/tags.service.ts
+++ src/app/core/services/tags/tags.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map, Observable } from "rxjs";
+import { IBlog } from "../../../libs/components/card/entity/card.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagsService {
+  private blogsUrl = 'api/data.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getTags$(): Observable<IBlog[]> {
+    return this.http.get<any>(this.blogsUrl).pipe(
+      map((data) => data?.tags)
+    );
+  }
 }
